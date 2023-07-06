e411c373051fca365f1777c87ce94596c24f8344
FerasAbushahla <abushahla21@gmail.com>
2023-07-06 22:35:59 +0300

    new API key

--- lib/services/apiSettings.dart
+++ lib/services/apiSettings.dart
@@ -1,5 +1,5 @@
 class ApiSettings{
-  static const String key = '14faccf8119f447a80e735b3b62f82b3';
+  static const String key = 'bf54a1f775c34a3698aef4220734cd76';
   //Base URL
   static const String baseUrl = 'https://api.weatherbit.io/v2.0/';
   static const String currentWeather = '${baseUrl}current?key=$key';
